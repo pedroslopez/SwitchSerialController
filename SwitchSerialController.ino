d6c6f46a6d70aa6c76313a3f28663c9897725d02
Pedro Lopez <pedroslopez@me.com>
2020-06-06 00:37:32 -0400

    Use binary to represent pressed buttons

--- SwitchSerialController.ino
+++ SwitchSerialController.ino
@@ -1,15 +1,12 @@
 #include <HID-Project.h>
 #include <HID-Settings.h>
 
-#define STICK_MIN      0
-#define STICK_CENTER 128
-#define STICK_MAX    255
-
 const byte numChars = 32;
 char receivedChars[numChars];
 char tempChars[numChars];
 
 char buttonData[numChars] = {0};
+uint16_t pressedButtons = 0;
 uint8_t leftStickX = 0;
 uint8_t leftStickY = 0;
 uint8_t rightStickX = 0;
@@ -20,7 +17,7 @@ boolean newData = false;
 void setup() {
   NSGamepad.begin();
   
-  Serial1.begin(9600);
+  Serial1.begin(115200);
   Serial1.println("<Arduino is ready>");
 }
 
@@ -33,7 +30,7 @@ void loop() {
     newData = false;
   }  
 
-  NSGamepad.loop();
+//  NSGamepad.loop();
 }
 
 void receiveData() {
@@ -73,7 +70,7 @@ void parseData() {
 
   // get buttons
   strtokIndx = strtok(tempChars,",");      
-  strcpy(buttonData, strtokIndx); 
+  pressedButtons = atoi(strtokIndx);
 
   // get left stick
   strtokIndx = strtok(NULL, ",");
@@ -91,15 +88,6 @@ void parseData() {
 }
 
 void sendControllerData() {
-  Serial1.print("Buttons ");
-  Serial1.println(buttonData);
-  Serial1.print("Left Stick ");
-  Serial1.println(leftStickX);
-  Serial1.println(leftStickY);
-  Serial1.print("Right Stick ");
-  Serial1.println(rightStickX);
-  Serial1.println(rightStickY);
-
   // Set sticks
   NSGamepad.leftXAxis(leftStickX);
   NSGamepad.leftYAxis(leftStickY);
@@ -107,92 +95,23 @@ void sendControllerData() {
   NSGamepad.rightYAxis(rightStickY);
 
   // Press/Release buttons
-  for(int i=0; buttonData[i] != 0; i++) {
-    switch(buttonData[i]) {
-      case 'a':
-        NSGamepad.press(NSButton_A);
-        break;
-      case 'A':
-        NSGamepad.release(NSButton_A);
-        break;
-      case 'b':
-        NSGamepad.press(NSButton_B);
-        break;
-      case 'B':
-        NSGamepad.release(NSButton_B);
-        break;
-      case 'y':
-        NSGamepad.press(NSButton_Y);
-        break;
-      case 'Y':
-        NSGamepad.release(NSButton_Y);
-        break;
-      case 'x':
-        NSGamepad.press(NSButton_X);
-        break;
-      case 'X':
-        NSGamepad.release(NSButton_X);
-        break;
-      case 'o':
-        NSGamepad.press(NSButton_LeftTrigger);
-        break;
-      case 'O':
-        NSGamepad.release(NSButton_LeftTrigger);
-        break;
-      case 'p':
-        NSGamepad.press(NSButton_RightTrigger);
-        break;
-      case 'P':
-        NSGamepad.release(NSButton_RightTrigger);
-        break;
-      case 'j':
-        NSGamepad.press(NSButton_LeftThrottle);
-        break;
-      case 'J':
-        NSGamepad.release(NSButton_LeftThrottle);
-        break;
-      case 'k':
-        NSGamepad.press(NSButton_RightThrottle);
-        break;
-      case 'K':
-        NSGamepad.release(NSButton_RightThrottle);
-        break;
-      case '-':
-        NSGamepad.press(NSButton_Minus);
-        break;
-      case '_':
-        NSGamepad.release(NSButton_Minus);
-        break;
-      case '+':
-        NSGamepad.press(NSButton_Plus);
-        break;
-      case '=':
-        NSGamepad.release(NSButton_Plus);
-        break;
-      case 'l':
-        NSGamepad.press(NSButton_LeftStick);
-        break;
-      case 'L':
-        NSGamepad.release(NSButton_LeftStick);
-        break;
-      case 'r':
-        NSGamepad.press(NSButton_RightStick);
-        break;
-      case 'R':
-        NSGamepad.release(NSButton_RightStick);
-        break;
-      case 'h':
-        NSGamepad.press(NSButton_Home);
-        break;
-      case 'H':
-        NSGamepad.release(NSButton_Home);
-        break;
-      case 'c':
-        NSGamepad.press(NSButton_Capture);
-        break;
-      case 'C':
-        NSGamepad.release(NSButton_Capture);
-        break;
-    }
-  } 
+  NSGamepad.buttons(pressedButtons);
+
+  // Serial reply
+  Serial1.print("BTN ");
+  Serial1.print(pressedButtons, BIN);
+
+  Serial1.print(" LX ");
+  Serial1.print(leftStickX, BIN);
+
+  Serial1.print(" LY ");
+  Serial1.print(leftStickY, BIN);
+
+  Serial1.print(" RX ");
+  Serial1.print(rightStickX, BIN);
+
+  Serial1.print(" RY ");
+  Serial1.println(rightStickY, BIN);
+
+  NSGamepad.write();
 }
